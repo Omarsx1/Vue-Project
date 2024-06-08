137c7bce2d0dbc4afb816a9add895ddc93f2b18f
Omar Salazar <salazarjuniorf@gmail.com>
2024-06-08 09:52:08 -0500

    Creación de componente contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const Aumentar = () => {
+  contador.value++;
+}
+const Disminuir = () => {
+  contador.value--;
+}
+</script>
+
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ contador }}</p>
+  <button id="aumentar" @click="Aumentar">Aumentar</button>
+  <button @click="Disminuir">Disminuir</button>
+</template>
+
+<style scoped>
+p {
+  font-size: 2rem;
+  color: red;
+}
+
+#aumentar {
+  display: inline;
+  margin-right: 10px;
+}
+</style>
